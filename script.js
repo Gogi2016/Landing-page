5452a9247912f2abb17bb58f3a6faa2fa5b9dcf5
Vuyokazi Gogi <138075341+Gogi2016@users.noreply.github.com>
2024-03-10 22:45:34 +0200

    Update script.js

--- script.js
+++ script.js
@@ -11,4 +11,24 @@ function updateSlider() {
     slider.style.transform = `translateX(${translateValue}%)`;
 }
 
-setInterval(nextSlide, 4000); 
+setInterval(nextSlide, 2000);
+
+function openPopup() {
+    document.getElementById('contactPopup').style.display = 'block';
+}
+
+function closePopup() {
+    document.getElementById('contactPopup').style.display = 'none';
+}
+
+function submitForm() {
+    var name = document.getElementById('name').value;
+    var email = document.getElementById('email').value;
+    var message = document.getElementById('message').value;
+
+    if (name.trim() === '' || email.trim() === '' || message.trim() === '') {
+        alert('Please fill in all the form fields before submitting.');
+    } else {
+        alert('Your message has been sent');
+    }
+}
